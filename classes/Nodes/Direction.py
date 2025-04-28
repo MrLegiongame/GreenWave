9af24129f5b8ff3fff7a41c737dab11be42aa815
Leonid <leonid.vyushkov99@gmail.com>
2025-04-28 23:56:13 +0300

    Integration

---
+++ classes/Nodes/Direction.py
@@ -0,0 +1,122 @@
+from classes.Nodes.Lane import Lane
+from screens.functions import find_obj_index_in_array
+from classes.Enums.LaneFacing import LaneFacing
+
+
+def check_possible_collision(left_lane, right_lane):
+    if not left_lane.to_lanes or not right_lane.to_lanes:
+        return  # No collision possible if no lanes to connect
+    if (left_lane.to_lanes[0]) < (right_lane.to_lanes[-1]):
+        raise TypeError("Direction is invalid: Possible collision between different lanes")
+
+
+def check_direction_validity(lanes):  # lanes: list[Lane]
+    if not isinstance(lanes, list):
+        raise TypeError("Direction is invalid: Not a list")
+    size = 0
+
+    for lane in lanes:
+        if not isinstance(lane, Lane):
+            raise TypeError("Direction is invalid: Non-Lane value")
+        if size >= 1:  # if at least the second lane (from the right)
+            check_possible_collision(left_lane=lane, right_lane=lanes[size - 1])
+        size += 1
+
+    if not size >= 1:
+        raise TypeError("Direction is invalid: No lanes")
+    return True
+
+
+class Direction:
+
+    def __init__(self, in_lanes=None, out_lanes=None):  # in_lanes: list[Lane], out_lanes: list[Lane]
+        self.in_lanes = []  # the logical order is from the right to the left
+        self.out_lanes = []  # the logical order is from the right to the left
+        self.in_size = 0
+        self.out_size = 0
+        self.size = 0
+        self.parent_junction = None
+        self.index_in_junction = None
+
+        if None is not in_lanes:
+            try:
+                check_direction_validity(in_lanes)
+                self.in_lanes = in_lanes
+                self.in_size = len(in_lanes)
+                self.size += self.in_size
+                self.__set_lanes(in_lanes)
+            except TypeError as e:
+                print(f"Direction couldn't be created due to this error: {e}")
+
+        if None is not out_lanes:
+            try:
+                check_direction_validity(out_lanes)
+                self.out_lanes = out_lanes
+                self.out_size = len(out_lanes)
+                self.size += self.out_size
+                self.__set_lanes(out_lanes)
+            except TypeError as e:
+                print(f"Direction couldn't be created due to this error: {e}")
+
+    def __set_lanes(self, lanes):
+        for lane in lanes:
+            lane.set_parent_direction(self)
+
+    def set_parent_junction(self, parent_junction):  # parent_junction: Junction
+        self.parent_junction = parent_junction
+        self.index_in_junction = find_obj_index_in_array(self, parent_junction.directions)
+
+    def add_to_left(self, lane):  # lane: Lane
+        if LaneFacing.IN == lane.facing:
+            self.in_lanes.append(lane)
+            return True
+        elif LaneFacing.OUT == lane.facing:
+            self.out_lanes.append(lane)
+            return True
+        return False
+
+    def add_to_right(self, lane):  # lane: Lane
+        if LaneFacing.IN == lane.type:
+            self.in_lanes.insert(0, lane)
+            return True
+        elif LaneFacing.OUT == lane.type:
+            self.out_lanes.insert(0, lane)
+            return True
+        return False
+
+    def find_index_by_lane(self, lane):  # lane: Lane
+        if not isinstance(lane, Lane):
+            return None
+        if LaneFacing.IN == lane.facing:
+            return find_obj_index_in_array(lane, self.in_lanes)
+        elif LaneFacing.OUT == lane.facing:
+            return find_obj_index_in_array(lane, self.out_lanes)
+        return None
+
+    def set_lane_as_direction_tuple(self, lane, index):  # lane: tuple[Direction], index: int
+        if not isinstance(self.direction, tuple):
+            raise TypeError(f"Setting lane {index} as tuple[Direction] is invalid: direction not a tuple")
+        if self.size <= index or 0 > index:
+            raise TypeError(f"Setting lane {index} as tuple[Direction] is invalid: index out of range")
+        if not isinstance(lane, tuple):
+            raise TypeError(f"Setting lane {index} as tuple[Direction] is invalid: given lane not a tuple")
+        for direction in lane:
+            if not isinstance(direction, Direction):
+                raise TypeError(f"Setting lane {index} as tuple[Direction] is invalid: direction not a Direction")
+        self.direction[index].lane = lane
+
+    def __str__(self):
+        """
+        if None is self.index_in_junction:
+            return None
+        res = f"Direction {self.index_in_junction}:\nIn Lanes:\n"
+        """
+        res = f"Direction:\nIn Lanes:\n"
+        for lane in self.in_lanes:
+            if isinstance(lane, Lane):
+                res += f"\t\t{lane}\n"
+        res += "\nOut Lanes:\n"
+        for lane in self.out_lanes:
+            if isinstance(lane, Lane):
+                res += f"\t\t{lane}\n"
+        return res
---
+++ classes/Nodes/TrafficLight.py
@@ -0,0 +1,52 @@
+from classes.Nodes.Lane import Lane
+from classes.Enums.State import State, STATE_SIZE
+from screens.functions import are_there_possible_collisions
+
+
+def check_traffic_light_validity(lanes):  # lanes: list[Lane]
+    """
+    Checks whether the values are valid (e.g. no future overflows nor possible collisions)
+    each lane of a traffic light is sorted from right (index 0) to left (index len(lanes) - 1)
+    """
+    if not isinstance(lanes, list):
+        raise TypeError("Traffic Light is invalid: Not a list parameter was given")
+    size = 0
+    for lane in lanes:
+        if not isinstance(lane, Lane):
+            raise TypeError("Traffic Light is invalid: Non-Lane value")
+        size += 1
+    if not (size >= 1):
+        raise TypeError("Traffic Light is invalid: Not enough lanes")
+    if are_there_possible_collisions(lanes):
+        raise TypeError("Traffic Light is invalid: Possible collisions detected")
+    return True
+
+
+class TrafficLight:
+
+    def __init__(self, lanes=None):
+        self.lanes = None
+        self.size = None
+        self.current_state = None
+
+        if None is not lanes:
+            self.set_lanes(lanes)
+
+    def set_lanes(self, lanes):
+        try:
+            check_traffic_light_validity(lanes)
+            self.lanes = lanes
+            self.size = len(lanes)
+        except TypeError as e:
+            print(f"Traffic Light couldn't be created due to this error: {e}")
+
+    def set_state(self, state):
+        if isinstance(state, State):
+            self.current_state = state
+            return True
+        return False
+
+    def change_state(self):
+        self.current_state = (self.current_state.value + 1) % STATE_SIZE
+        for lane in self.lanes:
+            lane.cur_state = self.current_state
